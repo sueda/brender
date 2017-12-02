01baf03851a9f96a4769a69d40e7b9e1d8b94898
Shinjiro Sueda <sueda@tamu.edu>
2017-12-02 06:58:02 -0600

    Fix for multiple subobjects with default names

--- src/Brenderable.h
+++ src/Brenderable.h
@@ -18,7 +18,7 @@ public:
 	Brenderable() {};
 	virtual ~Brenderable() {}
 	virtual int getBrenderCount() const { return 1; }
-	virtual std::vector<std::string> getBrenderNames() const { return std::vector<std::string>(1, ""); }
+	virtual std::vector<std::string> getBrenderNames() const { return std::vector<std::string>(getBrenderCount(), ""); }
 	virtual void exportBrender(std::vector< std::shared_ptr< std::ofstream > > outfiles) const = 0;
 private:
 
